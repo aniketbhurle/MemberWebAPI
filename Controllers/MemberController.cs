b323def9c2648318850643740fac4905e78d2056
aniketbhurle <bhurle.aniket@gmail.com>
2024-11-14 21:43:33 +0530

    Commiting the project

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Member.Data.Model;
+using Member.Data.Repository;
+namespace Member.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        public Repository_Member members = new Repository_Member();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Members>> GetAllMembersinList()
+        {
+            return members.GetAllMembers();
+        }
+
+        [HttpGet("{id}")]
+        public Members GetMemberbyID(int id)
+        {
+            return members.GetMemberId(id);
+        }
+
+        [HttpPost]
+        public void AddNewMember(Members mem)
+        {
+            members.AddNewMember(mem);
+            members.GetAllMembers();
+        }
+
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        [HttpDelete("{id}")]
+        public void DeleteMember(int id)
+        {
+            members.DeleteMember(id);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+namespace Member.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.Run();
+        }
+    }
+}
